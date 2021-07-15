7a4e11ea38de1aa0271d8767fb6b30ed62b451b1
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2021-07-16 00:26:30 +0530

    Create Climb Stairs With Variable Jumps Tabulation.cpp

---
+++ Dynamic Programming/Climb Stairs With Variable Jumps Tabulation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cs(int arr[], int n){
+    // write your code here
+    int dp[n]={0};
+    dp[n]=1;
+    for(int i=n-1;i>=0;i--){
+        for(int j=1;j<=arr[i];j++){
+            
+            if(i+j<=n){
+                dp[i]+=dp[i+j];
+            }
+        }
+        
+    }
+return dp[0];
+   
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ;i++){
+        cin>>arr[i];
+    }
+    cout<<cs(arr,n)<<endl;
+}
