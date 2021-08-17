ff211ed19241e7af42cc7f3d78603a33a50b8341
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2021-08-17 21:33:29 +0530

    Update dfs.cpp

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void bfs(int node,vector<int>&visited,vector<int>&res,vector<int>arr[]){
+void dfs(int node,vector<int>&visited,vector<int>&res,vector<int>arr[]){
     visited[node]=1;
     res.push_back(node);
     for(auto it:arr[node]){
         if(!visited[it])
-        bfs(it,visited,res,arr);
+        dfs(it,visited,res,arr);
     }
 }
-vector<int>CheckBfs(int n,int e,vector<int>arr[]){
+vector<int>CheckDfs(int n,int e,vector<int>arr[]){
     vector<int>visited(n+1,0);
     vector<int>res(n+1);
     for(int i=0;i<n;i++){
         if(!visited[i]){
-            bfs(i,visited,res,arr);
+            dfs(i,visited,res,arr);
         }
     }
     return res;
@@ -33,7 +33,7 @@ int main()
        arr[u].push_back(v);
        arr[v].push_back(u);
    }
-   for(auto it:CheckBfs(n,e,arr)){
+   for(auto it:CheckDfs(n,e,arr)){
        cout<<it<<" ";
    }
     return 0;
