21b92f5012a19ceee67223c4259c60e7ce669d65
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2021-08-24 16:00:47 +0530

    Update Level Order Traversal | BFS.cpp

--- Trees/Level Order Traversal | BFS.cpp
+++ Trees/Level Order Traversal | BFS.cpp
@@ -1,5 +1,18 @@
-//Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* root)
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+ vector<int> levelOrder(Node* root)
     {
       //Your code here
       vector<int>ans;
@@ -22,3 +35,19 @@
         }
         return ans;
     }
+int main()
+{
+    Node* root= new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(7);
+    root->left->left=new Node(3);
+    root->left->right=new Node(4);
+    root->left->right->left=new Node(5);
+    root->left->right->right=new Node(6);
+    vector<int>res=levelOrder(root);
+    for(auto it:res)
+    cout<<it<<" ";
+    return 0;
+}
+
+    
