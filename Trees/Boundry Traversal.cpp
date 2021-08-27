d014dca98da932d34f3e76430036f061b77ff849
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2021-08-27 21:44:40 +0530

    Create Boundry Traversal.cpp

---
+++ Trees/Boundry Traversal.cpp
@@ -0,0 +1,58 @@
+class Solution {
+void leftBoundry(Node* node,vector<int>&res){
+    while(true){
+        if(node->left){
+            res.push_back(node->data);
+            node=node->left;
+        }
+        else if(node->right){
+            res.push_back(node->data);
+            node=node->right;
+        }
+        else break;
+        
+    }
+}
+void leafNodes(Node* node,vector<int>&res){
+    if(!node->left && !node->right){
+       res.push_back(node->data);
+       return;
+    }
+    leafNodes(node->left,res);
+    leafNodes(node->right,res);
+}
+void RightBundry(Node* node,stack<int>&st){
+    node=node->right;
+    while(true){
+        if(node->right){
+            st.push(node->data);
+            node=node->right;
+        }
+        else if(node->left){
+            st.push(node->data);
+            node=node->left;
+        }
+        else break;
+        
+    }
+}
+public:
+    vector <int> printBoundary(Node *root)
+    {
+        //Your code here
+        
+        vector<int>res;
+        if(root==NULL)
+        return res;
+        leftBoundry(root,res);
+        leafNodes(root,res);
+        stack<int>st;
+        RightBundry(root,st);
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res;
+    }
+};
+
