c649af9f36e01a980e483c08076e91ba024e3a03
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2021-11-16 18:34:19 +0530

    Update Subset Sum Problem.cpp

--- Dynamic Programming/Subset Sum Problem.cpp
+++ Dynamic Programming/Subset Sum Problem.cpp
@@ -14,8 +14,15 @@ int dp[101][100001];
         }
         for(int i=0;i<N+1;i++){
             for(int j=0;j<sum+1;j++){
-                
+               if(arr[i-1]<=j)
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }  
             }
         }
+         return dp[N][sum];
     }
 };
