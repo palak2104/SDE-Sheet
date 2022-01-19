b1c9cfc6a14ea0156296553bda9b623f980044e1
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-20 01:44:18 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming (Striver)/Grid Unique Paths/Tabulation.cpp
@@ -0,0 +1,15 @@
+int uniquePaths(int m, int n) {
+	// Write your code here.
+    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    dp[1][1]=1;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+
+            if(i==1 && j==1) continue;
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+
+        }
+    }
+
+    return dp[m][n];
+}
