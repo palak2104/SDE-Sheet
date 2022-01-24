67c0f7b35c8483c66e1f2294fecdc9a4d3a3bb2c
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-24 23:03:41 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming (Striver)/Perfect Sum Problem/Tabulation.cpp
@@ -0,0 +1,27 @@
+class Solution{
+
+	public:
+	int perfectSum(int arr[], int n, int target)
+	{
+        // Your code goes here
+        long long mod=1e9+7;
+        int dp[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=target;j++){
+                if(i==0)
+                dp[i][j]=0;
+                if(j==0)
+                dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(j-arr[i-1]>=0)
+                dp[i][j]=dp[i-1][j-arr[i-1]]%mod+dp[i-1][j]%mod;
+                else dp[i][j]=dp[i-1][j]%mod;
+            }
+        }
+        return dp[n][target]%mod;
+	}
+	  
+};
