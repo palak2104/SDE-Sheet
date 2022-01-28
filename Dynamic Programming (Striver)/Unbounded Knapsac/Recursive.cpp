99cb69b68c11dac5208dcae6d54bb08bc5b36725
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-28 21:31:06 +0530

    Create Recursive.cpp

---
+++ Dynamic Programming (Striver)/Unbounded Knapsac/Recursive.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    int func(int i, int W, int val[], int wt[],vector<vector<int>>&dp){
+        if(i==0){
+            if(W-wt[i]>=0)
+            return val[i];
+            return 0;
+        }
+        //if(dp[i][W]!=-1) return dp[i][W];
+        int pick=INT_MIN;
+        if(W-wt[i]>=0)
+        pick=val[i]+func(i,W-wt[i],val,wt,dp);
+        int npick=func(i-1,W,val,wt,dp);
+        return max(pick,npick);
+    }
+public:
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        vector<vector<int>>dp(N+1,vector<int>(W+1,-1));
+        return func(N-1,W,val,wt,dp);
+        
+    }
+};
