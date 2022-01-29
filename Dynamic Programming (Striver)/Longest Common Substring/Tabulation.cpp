d6b3f7d1579eb6f9ca48dd8c14d85997a4719726
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-30 04:11:24 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming (Striver)/Longest Common Substring/Tabulation.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string a, string b, int n, int m)
+    {
+        // your code here
+        int maxi=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a[i-1]==b[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+                maxi=max(maxi,dp[i][j]);
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return maxi;
+    }
+};
