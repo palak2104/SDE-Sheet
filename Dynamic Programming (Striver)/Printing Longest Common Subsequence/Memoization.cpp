60b3b8285ba19695e1a057b8444bcbb7880c9f45
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-02-03 23:45:00 +0530

    Create Memoization.cpp

---
+++ Dynamic Programming (Striver)/Printing Longest Common Subsequence/Memoization.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string func(int i,int j,string a,string b,vector<vector<string>>&dp){
+    if(i==-1 || j==-1)
+    return "";
+    if(dp[i][j]!="-1")
+    return dp[i][j];
+    if(a[i]==b[j]){
+    string str="";
+    str+=a[i];
+    str+=func(i-1,j-1,a,b,dp);
+    return dp[i][j]=str;
+    }
+    string u=func(i-1,j,a,b,dp);
+    string v=func(i,j-1,a,b,dp);
+    if(u.length()>=v.length())
+    return dp[i][j]=u;
+    return dp[i][j]=v;
+}
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    vector<vector<string>>dp(a.length()+1,vector<string>(b.length()+1,"-1"));
+    string str=func(a.length()-1,b.length()-1,a,b,dp);
+    reverse(str.begin(),str.end());
+    cout<<str;
+    return 0;
+}
