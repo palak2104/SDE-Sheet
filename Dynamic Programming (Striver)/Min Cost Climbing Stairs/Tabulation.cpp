b4ef1e3a36eb1dd0c6bf00ff70f7c606366fe884
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-02-20 03:14:13 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming (Striver)/Min Cost Climbing Stairs/Tabulation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp(n+1);
+        dp[0]=arr[0];
+        dp[1]=arr[1];
+        for(int i=2;i<n;i++){
+            dp[i]=min(dp[i-1],dp[i-2])+arr[i];
+        }
+        return min(dp[n-1],dp[n-2]);
+    }
+};
