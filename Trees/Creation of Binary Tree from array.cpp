80bd3f2506497c90ec64b2d55492ffd2f4e3acde
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-03-29 00:46:25 +0530

    Create Creation of Binary Tree from array.cpp

---
+++ Trees/Creation of Binary Tree from array.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+void inorder(Node* head){
+    if(!head) return;
+    inorder(head->left);
+    cout<<head->data<<" ";
+    inorder(head->right);
+}
+void postorder(Node* head){
+    if(!head) return;
+    postorder(head->left);
+    postorder(head->right);
+    cout<<head->data<<" ";
+}
+void preorder(Node* head){
+    if(!head) return;
+    cout<<head->data<<" ";
+    preorder(head->left);
+    preorder(head->right);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    reverse(arr.begin(),arr.end());
+    Node* head=new Node(arr[arr.size()-1]);
+    queue<Node*>q;
+    q.push(head);
+    arr.pop_back();
+    while(arr.size()>=2){
+        int a=arr[arr.size()-1];
+        arr.pop_back();
+        int b=arr[arr.size()-1];
+        arr.pop_back();
+        Node* node=q.front();
+        q.pop();
+        Node* leftn=new Node(a);
+        Node* rightn=new Node(b);
+        node->left=leftn;
+        node->right=rightn;
+        q.push(leftn);
+        q.push(rightn);
+    }
+    if(arr.size()==1){
+    Node* node=q.front();    
+    int a=arr[arr.size()-1];
+    arr.pop_back();
+    Node* leftn=new Node(a);
+    node->left=leftn;
+    }
+    cout<<"Inorder traversal"<<endl;
+    inorder(head);
+    cout<<endl;
+    cout<<"Prerder traversal"<<endl;
+    preorder(head);
+    cout<<endl;
+    cout<<"Postorder traversal"<<endl;
+    postorder(head);
+    return 0;
+}
