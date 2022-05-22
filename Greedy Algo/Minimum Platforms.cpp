6e8cb1f6bdf8059e762e5bab8642557cf0a5cf02
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-05-23 04:28:24 +0530

    Create Minimum Platforms.cpp

---
+++ Greedy Algo/Minimum Platforms.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    sort(arr,arr+n);
+    sort(dep,dep+n);
+ 
+    int ans=INT_MIN;
+    int count=1;
+    int i=1,j=0;
+    while(i<n && j<n)
+    {
+        if(arr[i]<=dep[j]) //one more platform needed
+        {
+            count++;
+            i++;
+        }
+        else //one platform can be reduced
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count); //updating the value with the current maximum
+    }
+    return ans;
+    }
+};
